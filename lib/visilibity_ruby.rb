6316e2ad057544a4863f28d99cfd2484a005b06d
Brad Ediger <brad.ediger@madriska.com>
2009-05-10 00:36:57 -0500

    Cleaned up includes a bit; added inspectors for more classes

--- lib/visilibity_ruby.rb
+++ lib/visilibity_ruby.rb
@@ -6,21 +6,68 @@
 # pull in VisiLibity.(so|bundle|...)
 require 'VisiLibity'
 
-def Inspectors(*fake_ivars)
-  Module.new do
-    define_method :inspect do
-      ivar_str = fake_ivars.map{|iv| "@#{iv}=#{send(iv)}"}.join(" ")
-      "#<%s:0x%x %s>" % [self.class.name, object_id, ivar_str]
+module VisiLibity
+
+  # Enables enumeration over Swig collections implementing length() and index().
+  module SwigEnumerable
+    include Enumerable
+    def each
+      (0...size).each{|i| yield self.index(i)}
     end
   end
-end
 
-class VisiLibity::BoundingBox
-  include Inspectors(:x_min, :x_max, :y_min, :y_max)
-  # TODO: ctor
-end
+  def self.Inspectors(*fake_ivars)
+    Module.new do
+      define_method :inspect do
+        ivar_str = fake_ivars.map{|iv| "@#{iv}=#{send(iv).inspect}"}.join(" ")
+        "#<%s:0x%x %s>" % [self.class.name, object_id, ivar_str]
+      end
+    end
+  end
 
-class VisiLibity::Point
-  include Inspectors(:x, :y)
-end
+  class BoundingBox
+    include VisiLibity::Inspectors(:x_min, :x_max, :y_min, :y_max)
+    # TODO: ctor
+  end
 
+  class Point
+    include VisiLibity::Inspectors(:x, :y)
+  end
+
+  class LineSegment
+    include VisiLibity::Inspectors(:first, :second)
+  end
+
+  class Angle
+    include VisiLibity::Inspectors(:angle)
+    def angle
+      get()
+    end
+
+    def angle=(a)
+      set(a)
+    end
+  end
+
+  class PolarPoint
+    include VisiLibity::Inspectors(:range, :bearing)
+  end
+
+  class Ray
+    include VisiLibity::Inspectors(:base_point, :bearing)
+  end
+
+  class Polyline
+    include VisiLibity::SwigEnumerable
+    include VisiLibity::Inspectors(:vertices)
+
+    def vertices
+      to_a
+    end
+
+    def [](x)
+      index(x) # work around swig
+    end
+  end
+
+end
